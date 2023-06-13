7427301d89bb3f77fd57d18776cd537c2243846e
Luiz Guilherme S Seixas <guilhermesschiavon@gmail.com>
2023-06-13 14:35:35 -0300

    Realiza requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+    {
+      tableName: 'users',
+      timestamps: false,
+      underscored: true,
+    });
+
+  /*   User.associate = (models) => {
+      User.hasMany(models.BlogPost,
+        { foreignKey: 'userId', as: 'userPosts' });
+    }; */
+  return User;
+};
